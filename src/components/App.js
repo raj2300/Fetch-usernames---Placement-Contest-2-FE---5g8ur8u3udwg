b27b1ee340cc4c655e110753dc85cbb20ea7d441
RajVardhan Singh <71841397+raj2300@users.noreply.github.com>
2022-10-09 15:12:06 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
 import { useState, useEffect } from 'react';
+
+const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users";
+
 const App = () => {
- const[inputValue, setInputValue]=useState("");
- const[text, setText]= useState("");
- const changeInput = (e) =>{
-  setInputValue(e.target.value);
- }
- const buttonClick=()=>{
-  setText((prev)=>prev + inputValue);
-  setInputValue("");
- }
+  const [userId, setUserId] = useState("1");
+  const [name, setName] = useState("");
 
- 
+  useEffect(() => {
+    if (userId) fetch(`${BASE_URL}/${userId}`).then((res) => res.json()).then(user => setName(user.name))
+  }, [userId]);
 
-
+  const changeInput = (e) => {
+    setUserId(e.target.value);
+  }
 
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
-      <input id="input" onChange={changeInput} />
+      <input id="input" value={userId} onChange={changeInput} />
       <p id="name">{name}</p>
     </div>
   );
